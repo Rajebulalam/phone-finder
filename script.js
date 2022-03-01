83f2f87719c483370349d735c4d8e2c2b77d26e8
unknown <rajebul.alam17@gmail.com>
2022-03-01 17:37:38 +0600

    Display Phone on search Commited

--- script.js
+++ script.js
@@ -16,3 +16,30 @@ const searchPhone = () => {
             .then(data => displaySearchResult(data.data));
     }
 }
+
+
+// Display Phone 
+const displaySearchResult = phones => {
+    const searchResult = document.getElementById('search-result');
+    searchResult.textContent = '';
+    if(phones.length == 0 ){
+        alert('Sorry!! No Phone Found');
+    }else{
+        phones.slice(0, 20).forEach(phone => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+                <div class="card h-100 shadow p-3">
+                    <img src=" ${phone.image} " class="card-img-top h-75" alt="phone-img">
+                    <div class="card-body">
+                        <h5 class="card-title"> ${phone.phone_name} </h5>
+                        <p class="card-text"> ${phone.brand} </p>
+                        <button onclick = "loadPhoneDetails('${phone.slug}')" href="#" class="btn btn-success fw-bold">Details</button>
+                    </div>
+                </div>
+            `;
+            searchResult.appendChild(div);
+        });
+    }
+    
+}
